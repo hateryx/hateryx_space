2fc8ad0359894bfa4e35ceb333fd1294cb1fcbf8
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-07-06 08:14:29 +0800

    Added Full Stack Web Developer under Career section and revised certain aspects in other career milestones

--- src/components/constants/constCareersBody.js
+++ src/components/constants/constCareersBody.js
@@ -1,11 +1,19 @@
 const constCareersBody = [
   {
+    position: "Full Stack Web Developer",
+    company: "Enthusiast / Freelancer",
+    location: "Philippines",
+    period: "May 2022 - Current",
+    description:
+      "I have built and deployed multiple web applications, including an AI-powered web app and a full stack web app. Additionally, I have contributed to open-source projects on GitHub, delivering a feature to the Philippine Travel Level Map app. As part of my ongoing internship at Netzwelt PH, I have been enhancing my skills in database design using MySQL, C#, and overall software development expertise.",
+  },
+  {
     position: "Continuous Audit & Data Analytics Lead",
     company: "BDO Unibank",
     location: "Philippines",
-    period: "December 2022 - Current",
+    period: "December 2022 - June 2023",
     description:
-      "Using principles of data analytics, I work to develop automated processes for extracting datasets from system applications using SQL. I then transform this data using R Studio and Jupyter (Anaconda) and analyze it to identify anomalies and exceptions for endorsement to auditors for further continuous auditing and investigation.",
+      "I have developed automated processes for transforming large datasets extracted from system applications using SQL, R Studio, and Python libraries and encrypted it in a secure manner, and then analyzed it to identify anomalies and exceptions for endorsement to auditors for further continuous auditing and investigation.",
   },
   {
     position: "Section Head - IT Audit",
