aacea721f074a75d62ff1dffac213366f1fd352d
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-07-08 23:17:56 +0800

    Excluded carousel.css (customized)

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import '../assets/css/carousel.css'
 
 import { SVG_eye } from "../assets/icons";
 
